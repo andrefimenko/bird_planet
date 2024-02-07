b8f25e739f63e1976ea9e513587d2641e2a2eda7
andrefimenko <andrefimenko@yandex.ru>
2024-02-07 07:19:06 +0300

    Firing bullets

---
+++ bullet.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+
+    def __init__(self, bp_game):
+
+        super().__init__()
+        self.screen = bp_game.screen
+        self.settings = bp_game.settings
+        self.color = self.settings.bullet_color
+
+        self.rect = pygame.Rect(0,0 , self.settings.bullet_width,
+                                self.settings.bullet_height)
+        self.rect.midright = bp_game.ship.rect.midright
+
+        self.x = float(self.rect.x)
+
+    def update(self):
+
+        self.x += self.settings.bullet_speed
+
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+
+        pygame.draw.rect(self.screen, self.color, self.rect)
---
+++ settings.py
@@ -0,0 +1,17 @@
+class Settings:
+
+    def __init__(self):
+
+        # Screen settings
+        self.bg_color = (0, 0, 0)
+
+        # Ship settings
+        self.ship_vert_speed = 1.5
+        self.ship_hor_speed = 3
+
+        # Bullet settings
+        self.bullet_speed = 6
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = "white"
+        self.gun_fire_rate = 1
